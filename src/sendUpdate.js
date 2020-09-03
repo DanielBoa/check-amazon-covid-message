4fee29007bbb2209576b579c128f1d059d24e7c7
Daniel Boa <daniel.boa@gmail.com>
2020-09-03 12:44:22 +0100

    use message attributes correctly for aws.sns

--- src/sendUpdate.js
+++ src/sendUpdate.js
@@ -2,12 +2,19 @@ const aws = require('aws-sdk');
 const { phoneNumber, covidMessageUrl } = require('./config');
 const sns = new aws.SNS();
 
-const sendUpdate = message => sns
-  .setSMSAttributes({ attributes: { DefaultSenderID: 'update' } })
-  .promise()
-  .then(() => sns.publish({
-    Message: message + `\n\n${covidMessageUrl}`,
-    PhoneNumber: phoneNumber,
-  }).promise());
+const sendUpdate = message => sns.publish({
+  Message: message + `\n\n${covidMessageUrl}`,
+  PhoneNumber: phoneNumber,
+  MessageAttributes: {
+    'AWS.SNS.SMS.SMSType': {
+      DataType: 'String',
+      StringValue: 'Transactional',
+    },
+    'AWS.SNS.SMS.SenderID': { 
+      DataType: 'String',
+      StringValue: 'update',
+    },
+  },
+}).promise();
 
 module.exports = sendUpdate;
